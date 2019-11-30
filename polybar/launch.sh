9a0072e99974ff53bd517f3615c60d2c5db1f9ee
David Debreceni Jr <davidjr@debreceni.net>
2019-11-30 16:29:49 -0600

    Added some configuration things

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Terminate any running bar instances
-killall -q polyar
+killall -q polybar
 
 # Wait until completely terminated
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Lanch Polybar, using config in $HOME/.config/polybar/config
-polybar transparent &
+polybar -c ~/.config/polybar/config.ini top &
+polybar -c ~/.config/polybar/config.ini bottom &
 echo "Polybar launched..."
